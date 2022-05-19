1e16725d4e5d4ff90a368b70c504a7b5c6157d4e
ChrisLaRocque <larocque.christopher@gmail.com>
2022-05-19 12:24:19 -0400

    adds images

---
+++ gatsby-node.esm.js
@@ -0,0 +1,106 @@
+import { generateImageData } from "gatsby-plugin-image"
+import { getGatsbyImageResolver } from "gatsby-plugin-image/graphql-utils"
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+const fetch = require('node-fetch');
+
+// Image stuff
+const generateImageSource = (baseURL, width, height, format, quality) => {
+  const src = `${baseURL}?w=${width}&h=${height}&fmt=${format}&q=${quality || 80}`
+  return { src, width, height, format }
+}
+const resolveGatsbyImageData = async (image, options) => {
+	const chosenURL = image.urls.raw
+	const getURL = chosenURL.split('?')[0]
+	const params = new URLSearchParams(chosenURL.split('?')[1])
+	const format = params.get('fm')
+	const quality = params.get('q')
+	const parsedImage = {
+		src: getURL,
+		format:format,
+		quality: quality,
+		...image
+	}
+
+ const filename = parsedImage.src
+
+ const sourceMetadata = {
+	width: parsedImage.width,
+	height: parsedImage.height,
+	format: parsedImage.format,
+	quality: parsedImage.quality
+ }
+ const imageDataArgs = {
+	 ...options,
+	 // Passing the plugin name allows for better error messages
+	 pluginName: `gatsby-source-pain`,
+	 sourceMetadata,
+	 filename,
+	 generateImageSource,
+	 options,
+	 backgroundColor: parsedImage.color
+ }
+
+ return generateImageData(imageDataArgs)
+}
+export function createSchemaCustomization({actions}){
+	const { createTypes } = actions
+  createTypes(`
+    type DODDataItem implements Node {
+      image: UnsplashImage @link(from: "loopIndex", by: "loopIndex")
+    }
+  `)
+}
+export function createResolvers({ createResolvers }) {
+  createResolvers({
+    UnsplashImage: {
+      gatsbyImageData: getGatsbyImageResolver(resolveGatsbyImageData, {
+        quality: "Int",
+      }),
+    },
+  })
+}
+export async function sourceNodes({
+  actions: { createNode },
+  createContentDigest
+}){
+	const multiplier = 50
+	const imageFetch = await fetch(
+    `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_KEY}&page=1&per_page=${multiplier}&query=office`
+  );
+  const response = await fetch(
+    `https://www.defense.gov/data.json`
+  );
+	
+	// Resolve gatsby image data + create image nodes
+	const imageData = await imageFetch.json()
+	imageData.results.forEach((image, index)=>{
+		createNode({
+			...image,
+			loopIndex: index,
+			internal: {
+				type: 'UnsplashImage',
+				contentDigest: createContentDigest(image)
+			}
+		});
+	})
+	// Create nodes for dataset
+  const data = await response.json();
+	data.dataset.forEach((item, index)=>{
+		for(let i = 1; i < multiplier; i++){
+			createNode({
+				...item,
+				id: `${item.identifier}-${i}`,
+				loopIndex: i,
+				pageNumber: (index+1)*i,
+				internal: {
+					type: 'DODDataItem',
+					contentDigest: createContentDigest(item)
+				}
+			});
+		}
+	})
+
+};
--- src/pages/items/{DodDataItem.id}.jsx
+++ src/pages/items/{DodDataItem.id}.jsx
@@ -1,13 +1,42 @@
-import { graphql } from "gatsby";
 import React from "react";
-
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 export default function DodPage({ data }) {
-  console.log("data", data);
-  const { title, description } = data.dodDataItem;
+  const {
+    title,
+    description,
+    keyword,
+    modified,
+    publisher,
+    distribution,
+    image,
+  } = data.dodDataItem;
+
   return (
     <main>
+      <GatsbyImage image={image.gatsbyImageData} />
       <h1>{title}</h1>
+      <small>Modified: {modified}</small>
       <p>{description}</p>
+      <p>
+        <strong>Publisher: {publisher.name}</strong>
+      </p>
+      <p>
+        <strong>
+          Download URL: <a href={distribution[0].downloadURL}>Download</a>
+        </strong>
+      </p>
+
+      {keyword && (
+        <>
+          <p>Keywords</p>
+          <ul>
+            {keyword.map((word) => (
+              <li>{word}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </main>
   );
 }
@@ -16,6 +45,17 @@ export const query = graphql`
     dodDataItem(id: { eq: $id }) {
       title
       description
+      keyword
+      modified
+      publisher {
+        name
+      }
+      distribution {
+        downloadURL
+      }
+      image {
+        gatsbyImageData
+      }
     }
   }
 `;
